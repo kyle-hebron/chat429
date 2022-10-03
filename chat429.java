495c345d2209a20c411473bbbb343262dbeec3c3
Kyle_Hebron <thewaffleminister@gmail.com>
2022-10-02 20:53:50 -0700

    Temp

--- chat429.java
+++ chat429.java
@@ -4,6 +4,6 @@ import java.util.*;
 public class chat429{
     public static void main(String args[]){
         System.out.println("Hello world");
-        int peanut;
+        int peanut = 0;
     }
 }
